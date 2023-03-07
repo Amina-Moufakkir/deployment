4b34686d1ee663f7bb340ee3f4fb9cd20e8fc7f4
Amina-Moufakkir <aminamouf22@gmail.com>
2023-03-07 12:23:09 -0500

    more changes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
       <h1>Pursuit React Template</h1>
       <p>My new update!</p>
       <p>Changes on my new branch</p>
+      <h4>Making more changes on my new branch</h4>
+      <p></p>
     </div>
   );
 }
